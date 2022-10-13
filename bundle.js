deec52f515e943378f3ebe941a68225db4aac167
Zohan Subhash <zohan.subhash@gmail.com>
2022-10-13 13:26:18 +0530

    Commit

--- bundle.js
+++ bundle.js
@@ -4,19 +4,47 @@
 const newtonMethod = require('newton-method')
 
 document.getElementById('button').onclick = () => {
+    let iterations
+    let initial
+    if (isNaN(document.getElementById('iterations').value !== 'number') || !document.getElementById('iterations').value) {
+        iterations = 25
+    } else {
+        iterations = document.getElementById('iterations').value
+    }
+    if (isNaN(document.getElementById('initial').value !== 'number') || !document.getElementById('initial').value) {
+        initial = 25
+    } else {
+        initial = document.getElementById('initial').value
+    }
     if (!document.getElementById('text').value) {
         document.getElementById('result').innerHTML = 'Please enter a function!'
     } else {
-        document.getElementById('result').innerHTML = `Result: ${newtonMethod(document.getElementById('text').value, 25, document.getElementById('initial').value || 0)}`
+        document.getElementById('result').innerHTML = `Result: ${newtonMethod(document.getElementById('text').value, iterations, initial)}`
     }
 }
 
 document.getElementById('cleartext').onclick = () => {
     document.getElementById('result').innerHTML = ''
     document.getElementById('text').value = ''
+    document.getElementById('initial').value = ''
+    document.getElementById('iterations').value = ''
 }
 
-input.addEventListener("keydown", function (event) {
+document.getElementById('text').addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault()
+        document.getElementById("button").click()
+    }
+})
+
+document.getElementById('initial').addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault()
+        document.getElementById("button").click()
+    }
+})
+
+document.getElementById('iterations').addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         event.preventDefault()
         document.getElementById("button").click()
